165fe96ae0ba10a73d817c2055607cdf2c25e423
Dimirvan <info.dimirvan@gmail.com>
2024-09-28 17:54:45 +0330

    debug

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,6 +7,9 @@ function Home(){
             <Navbar />
             <SubHeader />
             <OurCourses />
+            <div>
+                amirreza
+            </div>
         </>
     )
 }
