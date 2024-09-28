f07ebed87949915579af2242edc3a7cd4e540c6c
Dimirvan <info.dimirvan@gmail.com>
2024-09-28 18:00:02 +0330

    add section why Us to project

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../../components/navbar/Navbar"
 import SubHeader from "../../components/sub-header/SubHeader"
 import OurCourses from '../../components/our-courses/OurCourses'
+import WhyUs from "../../components/Why_Us/WhyUs"
 function Home(){
     return(
         <>
             <Navbar />
             <SubHeader />
+            <WhyUs />
             <OurCourses />
             <div>
                 amirreza
